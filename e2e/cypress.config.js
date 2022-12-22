307b9df1db27a3cb122883b68cc94c207389cb0a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-12-22 06:59:15 -0500

    use project path

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   e2e: {
     supportFile: false,
     fixturesFolder: false,
-    specPattern: 'e2e/cypress/integration/*.cy.js',
+    specPattern: 'cypress/integration/*.cy.js',
   },
 })
